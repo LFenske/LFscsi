423632f75aa6f5d3091d3c1e6d7929aea1e2c6da
larry <>
2005-09-09 19:58:06 +0000

    add TestUnitReady

---
+++ ScsiCmd/CmdTestUnitReady.c
@@ -0,0 +1,70 @@
+
+
+#ifdef ENUM
+  CMD_TESTUNITREADY,
+#endif
+
+
+#ifdef DEF
+  {CMD_TESTUNITREADY, "testunitready", LineTestUnitReady, DIRECTION_NONE, NULL, "", ""},
+  {CMD_TESTUNITREADY, "tur"          , LineTestUnitReady, DIRECTION_NONE, NULL, "", ""},
+#endif
+
+
+#ifdef LINE
+#include "CmdTestUnitReady.h"
+
+int
+LineTestUnitReady(SCSI_HANDLE handle, COMMON_PARAMS common,
+            int argc, char**argv)
+{
+  if (argc > 0) {
+    /*stub: usage(progname);*/
+    return -1;
+  }
+
+  {
+    dat = CmdTestUnitReady(handle, common);
+  }
+  return 0;
+}
+#endif
+
+
+#ifdef COMMAND
+#define __CmdTestUnitReady_c
+#include "CmdTestUnitReady.h"
+#undef  __CmdTestUnitReady_c
+
+#include "ScsiTransport.h"
+#include "common.h"
+#include <stdlib.h>   /* for malloc */
+
+VECTOR
+CmdTestUnitReady(SCSI_HANDLE handle, COMMON_PARAMS common)   /* timeout */
+{
+  byte cdb[6];
+  VECTOR cdbvec;
+  VECTOR retval;
+
+  cdbvec.dat = cdb;
+  cdbvec.len = sizeof(cdb);
+  retval.dat = NULL;
+  retval.len = 0;
+
+  cdb[0] = 0x00;
+  cdb[1] = 0;
+  cdb[2] = 0;
+  cdb[3] = 0;
+  cdb[4] = 0;
+  cdb[5] = 0;
+  send_cdb(handle, common,
+	   DIRECTION_NONE,
+	   cdbvec,
+	   retval,
+	   1.);
+  return retval;
+}
+#endif
+
+
---
+++ ScsiCmd/CmdTestUnitReady.h
@@ -0,0 +1,12 @@
+#ifndef __CmdTestUnitReady_h
+#define __CmdTestUnitReady_h
+
+
+#include "common.h"
+
+
+VECTOR
+CmdTestUnitReady(SCSI_HANDLE handle, COMMON_PARAMS common);   /* timeout */
+
+
+#endif /* __CmdTestUnitReady_h */
