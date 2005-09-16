b458d1874b4776f8affdab587c6de2b8fea09221
larry <>
2005-09-16 19:39:10 +0000

    add -s to force interpretation as standard page

--- ScsiCmd/PrintInquiry.c
+++ ScsiCmd/PrintInquiry.c
@@ -1,22 +1,42 @@
 #include "common.h"
 #include "PrintInquirySub.h"
 #include <stdlib.h>   /* for malloc */
-#include <unistd.h>   /* for read */
+#include <unistd.h>   /* for read, getopt */
 
 
 int
-main()
+main(int argc, char**argv)
 {
   VECTOR dat;
   char *dp;
   int left = 0x10000;
   int thislen;
+  bool force_stdpage = FALSE;
+
+  {
+    int ch;
+    while ((ch = getopt(argc, argv, "s")) != -1) {
+      switch (ch) {
+      case 's': force_stdpage = TRUE; break;
+      case '?':
+      default:
+        {
+          /*stub: usage(progname);*/
+          exit(-1);
+        }
+        break;
+      }
+    }
+  }
+
   dp = dat.dat = malloc(left);
   while ((thislen = read(0, dp, left))) {
     dp   += thislen;
     left -= thislen;
   }
   dat.len = dp - (char*)dat.dat;
+  if (force_stdpage && dat.dat[2] == 0)
+    dat.dat[2] = 0xff;
   PrintInquirySub(dat);
   free(dat.dat);
   return 0;
