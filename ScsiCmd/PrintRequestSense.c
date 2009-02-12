d5c5ba06c07bf47006cae2e5b28ce6d2c937493b
larry <>
2009-02-12 22:25:32 +0000

    add LGPL and copyright

--- ScsiCmd/PrintRequestSense.c
+++ ScsiCmd/PrintRequestSense.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2008  Larry Fenske
+ * 
+ * This file is part of LFscsi.
+ * 
+ * LFscsi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * LFscsi is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with LFscsi.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "common.h"
 #include "PrintRequestSenseSub.h"
 #include <stdlib.h>   /* for malloc */
